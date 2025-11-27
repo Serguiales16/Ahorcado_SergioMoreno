801410f2b60c11837377c3e20c335fdf2824798a
serguiales <serguiales@gmail.com>
2025-11-27 11:32:17 +0100

    Logica aplicada (falta de imagen)

--- app/src/main/java/com/example/ahorcado_sergiomoreno/MainActivity.kt
+++ app/src/main/java/com/example/ahorcado_sergiomoreno/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.ahorcado_sergiomoreno
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ahorcado_sergiomoreno.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +27,60 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        fun asignacion() {
+
+            val palabras = arrayOf(
+                "C,A,S,A,S",
+                "P,E,R,R,O",
+                "G,A,T,O,S",
+                "J,U,E,G,O",
+                "S,A,L,O,N",
+                "P,L,A,Y,A",
+                "T,E,C,L,A",
+                "R,E,D,E,S",
+                "C,L,A,S,E",
+                "L,I,B,R,O",
+                "C,O,D,I,G",
+                "A,H,O,R,C",
+                "N,U,B,E,S",
+                "P,I,E,D,A",
+                "V,I,D,E,O",
+                "M,O,U,S,E",
+                "P,I,X,E,L",
+                "S,O,N,I,D",
+                "F,O,R,M,A",
+                "L,O,G,I,C"
+            )
+
+            val palabraLetras = (palabras[Random.nextInt(palabras.size)].split(","))
+
+
+            //   var palabraLetras = arrayOf(palabras[Random.nextInt(palabras.size)].split(","))
+
+            binding.letra1.visibility = View.INVISIBLE
+            binding.letra2.visibility = View.INVISIBLE
+            binding.letra3.visibility = View.INVISIBLE
+            binding.letra4.visibility = View.INVISIBLE
+            binding.letra5.visibility = View.INVISIBLE
+
+            binding.letra1.text = palabraLetras[0].toString()
+            binding.letra2.text = palabraLetras[1].toString()
+            binding.letra3.text = palabraLetras[2].toString()
+            binding.letra4.text = palabraLetras[3].toString()
+            binding.letra5.text = palabraLetras[4].toString()
+
+        }
+
+
+
+        binding.enviar.setOnClickListener {
+
+            var letraSeleccionada = binding.letra.text.toString().uppercase()
+
+
+
+
+        }
 
     }
 }
